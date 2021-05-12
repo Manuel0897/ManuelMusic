4f10d36cd14ed6b5cc19c14521d0bf7f34c0475b
Manuel Mendoza <manuel_mm97@hotmail.com>
2021-05-12 18:02:40 -0500

    music Service

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from "@angular/core";
+import { ModalController } from "@ionic/angular";
+import { MusicService } from '../services/music.service';
+import { SongsModalPage } from '../songs-modal/songs-modal.page';
 
 @Component({
   selector: "app-home",
@@ -7,4 +10,28 @@ import { Component } from "@angular/core";
 })
 export class HomePage {
   constructor() {}
+  slideOps = {
+    initialSlide: 2,
+    slidesPerView: 4,
+    centeredSlides: true,
+    speed: 400
+  };
+
+  artists: any[] = [];
+  songs: any[] = [];
+  albums: any[] = [];
+  
+  constructor(
+    private musicService: MusicService,
+    private modalCtrl: ModalController
+  ) {}
+
+  ionViewDidEnter() {
+    this.musicService.getNewReleases().then(newReleases => {
+      this.artists = this.musicService.getArtists();
+      this.songs = newReleases.albums.items.filter(e => e.album_type == "single");
+      this.albums = newReleases.albums.items.filter(e => e.album_type == "album");
+    })
+  }
+
 }
---
+++ src/app/services/music.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import * as dataArtists from './artists.json';
+import * as dataReleases from './releases.json';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MusicService {
+
+  constructor() { }
+
+  getArtists() {
+    return dataArtists.items;
+  }
+
+  getNewReleases() {
+    return fetch("https://platzi-music-api.herokuapp.com/browse/new-releases")
+      .then(res => res.json())
+      .catch(() => dataReleases);
+  }
+
